22e9c6f944719d893221596e003a5abf12f9622c
Jeremías <jrmsdev@gmail.com>
2017-12-21 20:00:09 -0300

    do some checks

--- jcms/jcms.go
+++ jcms/jcms.go
@@ -2,19 +2,37 @@
 package jcms
 
 import (
+    "log"
     "github.com/jrmsdev/go-jcms/internal/httpd"
+    "github.com/jrmsdev/go-jcms/internal/config"
     "github.com/jrmsdev/go-jcms/internal/webapps"
 )
 
+var listening = false
+
 func Listen () string {
-    return httpd.Listen ()
+    log.Printf ("%s version %s\n", config.NAME, Version ())
+    uri := httpd.Listen ()
+    log.Println ("URI:", uri)
+    listening = true
+    return uri
 }
 
 func Serve () {
-    webapps.Start ()
-    httpd.Serve ()
+    if listening {
+        webapps.Start ()
+        httpd.Serve ()
+    } else {
+        log.Fatalln ("E: call jcms.Listen() first")
+    }
 }
 
 func Stop () {
-    httpd.Stop ()
+    if listening {
+        httpd.Stop ()
+        listening = false
+    } else {
+        log.Fatalln ("E: trying to stop a not listening server...")
+        log.Fatalln ("E: jcms.Listen() and jcms.Server() should be called first")
+    }
 }
