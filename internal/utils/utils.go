bce8fa36d1be54b5e4fa3ad8eb1cc5f68a970fbb
Jeremías <jrmsdev@gmail.com>
2017-12-21 20:37:23 -0300

    internal utils

---
+++ internal/utils/utils.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+    "os"
+)
+
+func DirExists (p string) bool {
+    if f, err := os.Stat (p); os.IsNotExist (err) {
+        return false
+    } else {
+        return f.IsDir ()
+    }
+}
+
+func FileExists (p string) bool {
+    if f, err := os.Stat (p); os.IsNotExist (err) {
+        return false
+    } else {
+        return !f.IsDir ()
+    }
+}
