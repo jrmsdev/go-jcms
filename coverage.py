6ac72bcd4134ff8b15728db142406b95a8d9a4c7
Jeremías <jrmsdev@gmail.com>
2017-12-29 00:28:02 -0300

    tests coverage HTML report

---
+++ coverage.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python3
+
+import os
+from os import path
+from subprocess import check_output, check_call
+
+GOPATH = os.getenv ('GOPATH')
+
+HTML_HEAD = '''
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<style>
+			body {
+				background: black;
+				color: rgb(80, 80, 80);
+			}
+			body, pre, #legend span {
+				font-family: Menlo, monospace;
+				font-weight: bold;
+			}
+			#topbar {
+				background: black;
+				position: fixed;
+				top: 0; left: 0; right: 0;
+				height: 42px;
+				border-bottom: 1px solid rgb(80, 80, 80);
+			}
+			#content {
+				margin-top: 50px;
+			}
+			#nav, #legend {
+				float: left;
+				margin-left: 10px;
+			}
+			#legend {
+				margin-top: 12px;
+			}
+			#nav {
+				margin-top: 10px;
+			}
+			#legend span {
+				margin: 0 5px;
+			}
+            .cov0 { color: rgb(192, 0, 0) }
+            .cov1 { color: rgb(128, 128, 128) }
+            .cov2 { color: rgb(116, 140, 131) }
+            .cov3 { color: rgb(104, 152, 134) }
+            .cov4 { color: rgb(92, 164, 137) }
+            .cov5 { color: rgb(80, 176, 140) }
+            .cov6 { color: rgb(68, 188, 143) }
+            .cov7 { color: rgb(56, 200, 146) }
+            .cov8 { color: rgb(44, 212, 149) }
+            .cov9 { color: rgb(32, 224, 152) }
+            .cov10 { color: rgb(20, 236, 155) }
+        </style>
+        <title>JCMS Tests Coverage</title>
+	</head>
+	<body>
+        <div id="content">
+            <pre class="file">
+'''
+
+HTML_TAIL = '''
+            </pre>
+        </div>
+    </body>
+</html>
+'''
+
+COVDONE = '''  <a class="cov10" href="{}">{}</a>'''
+COVMISS = '''  <span class="cov0">{}</span>'''
+
+def testcover (pkg):
+    oldwd = os.getcwd ()
+    os.chdir (path.join (GOPATH, 'src', pkg))
+    fh = open (os.devnull, 'w')
+    check_call ('go test -coverprofile coverage.out'.split (),
+            stderr = fh)
+    fh.close ()
+    if path.isfile ('coverage.out'):
+        check_call ('go tool cover -html coverage.out -o coverage.html'.split ())
+        covdone (pkg)
+    else:
+        covmiss (pkg)
+    os.chdir (oldwd)
+
+def covdone (pkg):
+    covhtml = path.join (GOPATH, 'src', pkg, 'coverage.html')
+    print (COVDONE.format (covhtml, pkg), file = INDEX_FH)
+
+def covmiss (pkg):
+    print (COVMISS.format (pkg), file = INDEX_FH)
+
+if __name__ == '__main__':
+    global INDEX_FH
+    INDEX_FH = open ('coverage.html', 'w')
+    print (HTML_HEAD, file = INDEX_FH)
+    for pkg in check_output ('go list ./...'.split ()).decode ().splitlines ():
+        testcover (pkg)
+    print (HTML_TAIL, file = INDEX_FH)
+    INDEX_FH.close ()
