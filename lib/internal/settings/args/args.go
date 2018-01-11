46c57d8c2d98940a00e920395d4ee164e1449505
Jeremías <jrmsdev@gmail.com>
2018-01-11 05:39:44 -0300

    settings args tests

--- lib/internal/settings/args/args.go
+++ lib/internal/settings/args/args.go
@@ -1,5 +1,9 @@
 package args
 
+import (
+	"fmt"
+)
+
 type Args struct {
 	prefix string            `json:"omit"`
 	Args   map[string]string `json:",omitempty"`
@@ -10,9 +14,16 @@ func (a *Args) SetPrefix(prefix string) {
 }
 
 func (a *Args) Get(key, defval string) string {
-	v, ok := a.Args[key]
+	v, ok := a.Args[a.getKey(key)]
 	if ok {
 		return v
 	}
 	return defval
+}
+
+func (a *Args) getKey(key string) string {
+	if a.prefix != "" && a.prefix != "." {
+		return fmt.Sprintf("%s.%s", a.prefix, key)
+	}
+	return fmt.Sprintf("%s", key)
 }
---
+++ lib/internal/settings/args/args_test.go
@@ -0,0 +1,44 @@
+package args
+
+import (
+	"testing"
+)
+
+func TestArgs(t *testing.T) {
+	a := &Args{}
+	a.Args = map[string]string{
+		"arg0": "val0",
+		"arg1": "val1",
+		"testing.prefix.arg0": "val0",
+	}
+	testGet(t, a)
+	testPrefix(t, a)
+}
+
+func testGet(t *testing.T, a *Args) {
+	v := a.Get("arg0", "GETERROR")
+	if v != "val0" {
+		t.Error("invalid arg value for arg0:", v)
+	}
+	v = a.Get("arg", "DEFVAL")
+	if v != "DEFVAL" {
+		t.Error("invalid arg default value:", v)
+	}
+}
+
+func testPrefix(t *testing.T, a *Args) {
+	a.SetPrefix("testing.prefix")
+	v := a.Get("arg0", "GETERROR")
+	if v != "val0" {
+		t.Error("invalid arg value for arg0:", v)
+	}
+	v = a.Get("arg", "DEFVAL")
+	if v != "DEFVAL" {
+		t.Error("invalid arg default value:", v)
+	}
+	v = a.Get("arg1", "NOTFOUND")
+	if v != "NOTFOUND" {
+		t.Error("prefixed arg1 key should be not found:", v)
+	}
+	a.SetPrefix("")
+}
